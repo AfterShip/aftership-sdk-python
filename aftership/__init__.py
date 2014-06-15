208c89937f098b2068e3003081a5efb4a9151b68
Fedor Korshunov <mail@fedor.cc>
2014-06-15 07:19:00 +0700

    Included API v3 specifics. Custom error handle type, and API class — APIv3.
    APIv3 main features:
    — Returns 'data' dictionary on success (strips 'meta dictionary').
    — Accepts lists and datetime objects as input parameters (converts them to comma-separated strings and ISO 8601 strings).
    — On return, converts ISO 8601 strings to datetime.

--- aftership/__init__.py
+++ aftership/__init__.py
@@ -1,14 +1,30 @@
 import json
 import time
 import threading
+import datetime
 import requests
+import dateutil.parser
 
 __author__ = 'Fedor Korshunov <mail@fedor.cc>'
 
 
-class AftershipRequestException(Exception):
+class APIRequestException(Exception):
     def __getitem__(self, attribute):
-        return self.message[attribute]
+        return self.args[0][attribute]
+
+
+class APIv3RequestException(APIRequestException):
+    def code(self):
+        return self['meta']['code']
+
+    def type(self):
+        return self['meta']['error_type']
+
+    def message(self):
+        return self['meta']['error_message']
+
+    def data(self):
+        return self['data']
 
 
 class RequestPart(object):
@@ -40,11 +56,9 @@ class RequestPart(object):
 
 class API(RequestPart):
     def __init__(self, key=None,
-                 max_calls_per_sec=10.0,
+                 max_calls_per_sec=10,
                  base_url='https://api.aftership.com',
-                 ver='v3',
-                 headers={}):
-
+                 ver='v3', headers={}):
         self._last_call = None
         self._rate_limit = 1.0 / float(max_calls_per_sec)
 
@@ -62,10 +76,11 @@ class API(RequestPart):
         headers = self._headers
         if method != 'get':
             headers['Content-Type'] = 'application/json'
-            params = {}
+            params = None
+            body = json.dumps(body)
         elif body:
             params = body
-            body = {}
+            body = None
 
         with threading.Lock():
             if self._last_call:
@@ -74,13 +89,72 @@ class API(RequestPart):
                     time.sleep(delta)
             self._last_call = time.clock()
 
-        response = requests.request(method, url,
-                                    headers=headers,
-                                    params=params,
-                                    data=json.dumps(body))
+        response = requests.request(method, url, headers=headers,
+                                    params=params, data=body)
         ret = json.loads(response.text)
 
         if not response.ok:
-            raise AftershipRequestException(ret)
+            raise APIRequestException(ret)
 
         return ret
+
+
+class APIv3(API):
+    def __init__(self, key, max_calls_per_sec=10, datetime_convert=True):
+        self._datetime_fields = ['created_at',
+                                 'created_at_min',
+                                 'created_at_max',
+                                 'updated_at',
+                                 'expected_delivery',
+                                 'checkpoint_time']
+        self._datetime_convert = datetime_convert
+        API.__init__(self, key, max_calls_per_sec=max_calls_per_sec,
+                     base_url='https://api.aftership.com',
+                     ver='v3', headers={})
+
+    def _is_datetime(self, key, value):
+        if type(value) is unicode and key in self._datetime_fields:
+            return True
+        return False
+
+    def _convert_datetime_dict(self, dct):
+        for key, value in dct.iteritems():
+
+            # Convert ISO 8601 strings to datetime
+            if self._is_datetime(key, value):
+                dct[key] = dateutil.parser.parse(value)
+
+            # Iterate thru dict
+            elif type(value) is dict:
+                dct[key] = self._convert_datetime_dict(value)
+
+            # Iterate thru list
+            elif type(value) is list:
+                dct[key] = []
+                for item in value:
+                    dct[key].append(self._convert_datetime_dict(item))
+
+        return dct
+
+    def call(self, *args, **body):
+        try:
+            for key, value in body.iteritems():
+                # Convert datetime to ISO 8601 string
+                if type(value) is datetime.datetime:
+                    value = value.replace(microsecond=0)
+                    body[key] = value.isoformat()
+
+                # Convert array of values to comma-separated string
+                elif type(value) is list:
+                    body[key] = u','.join(value)
+
+            response = API.call(self, *args, **body)['data']
+
+            # Convert ISO 8601 strings to datetime
+            if self._datetime_convert:
+                self._convert_datetime_dict(response)
+
+            return response
+        except APIRequestException as error:
+            raise APIv3RequestException(*error.args)
+
