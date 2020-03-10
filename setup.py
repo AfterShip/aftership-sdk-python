40e8849eae9a799ae96c0dff1381ebbca0503727
Alvie Zhang <alvie.zhang@gmail.com>
2020-03-10 09:19:28 +0800

    Fix import problem

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
+import codecs
 import os
+import re
 from codecs import open  # To use a consistent encoding
 
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    print(version_file)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 # Get the long description from the relevant file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -12,8 +30,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     install_requires = f.read().split('\n')
 
-with open(os.path.join(here, 'VERSION'), encoding='utf-8') as f:
-    __version__ = f.read().strip()
+__version__ = find_version("aftership", "__init__.py")
 
 setup(
     name='aftership',
